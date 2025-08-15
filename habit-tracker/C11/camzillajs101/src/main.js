2b18163fb146c46eb592cce240ba3be4a0a7920d
Camilo Mason <camilomason0@gmail.com>
2025-08-15 16:08:41 +0000

    Add JS for habit creation and local storage

--- habit-tracker/C11/camzillajs101/src/main.js
+++ habit-tracker/C11/camzillajs101/src/main.js
@@ -1,4 +1,5 @@
 const form = document.getElementById('new_habit');
+let habits;
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -10,5 +11,22 @@ form.addEventListener('submit', (e) => {
         streak: parseInt(data.get('target_streak'))
     };
 
-    console.log(habit)
+    habits.push(habit);
+    renderHabits(habits);
+    window.localStorage.setItem('habits', JSON.stringify(habits));
+});
+
+const renderHabits = habits => {
+    const habits_container = document.getElementById('habits_container');
+
+    habits_container.innerHTML = habits.map(habit => {
+        return `<section class="habit">
+        <h3>${habit.name}</h3><h4>${habit.streak}</h4>
+        </section>`
+    }).join("\n");
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    habits = JSON.parse(localStorage.getItem('habits')) ?? [];
+    if (habits !== null) { renderHabits(habits) }
 });
